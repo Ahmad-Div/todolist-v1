7a87232ece52c8b3f4909648033b5003e65826a4
Ahmad Software <dr.ahmad.salah.54@gmail.com>
2022-08-01 10:11:35 +0300

    final commit add all

---
+++ date.js
@@ -0,0 +1,9 @@
+exports.getDate = function () {
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  return new Date().toLocaleDateString("en-US", options);
+};
